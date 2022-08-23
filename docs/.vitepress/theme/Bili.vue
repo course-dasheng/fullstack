47698b5f67e838d8a8da68b433ba99340fff9350
花果山大圣 <316783812@qq.com>
2022-08-23 10:28:02 +0800

    update

---
+++ docs/.vitepress/theme/Bili.vue
@@ -0,0 +1,18 @@
+<template>
+  <iframe :src="src" frameborder="0"></iframe>
+</template>
+
+<script>
+export default{
+  props: ['id'],
+  computed:{
+    src(){
+      return 'https://www.bilibili.com/video/'+this.id
+    }
+  },
+  mouted(){
+    console.log(this.props)
+  }
+
+}
+</script>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,6 @@
 import Theme from 'vitepress/theme'
 import Mermaid from 'vitepress-plugin-mermaid/Mermaid.vue'
-
+import Bili from './Bili.vue'
 // import Antd from 'ant-design-vue';
 // import 'ant-design-vue/dist/antd.css';
 import './index.css'
@@ -10,5 +10,6 @@ export default {
   enhanceApp({ app }) {
     // app.use(Antd)
     app.component('Mermaid', Mermaid)
+    app.component('Bili', Bili)
   },
 }
